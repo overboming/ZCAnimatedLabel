98bdc1a9f5e0717523b4dfccf81991388c194a96
Chen Zhang <overboming@gmail.com>
2015-03-17 21:33:46 +0800

    remove CA3DTransform to CGImage impelementation, which isn't worth it

--- ZCAnimatedLabel/ZCSpinLabel.m
+++ ZCAnimatedLabel/ZCSpinLabel.m
@@ -5,15 +5,11 @@
 //  Created by Chen Zhang on 3/17/15.
 //  Copyright (c) 2015 alipay. All rights reserved.
 //
-//  3d transform renderer from AGGeometryKit
-//  http://stackoverflow.com/questions/1949003/how-do-i-create-render-a-uiimage-from-a-3d-transformed-uiimageview
-//
 //  3d transform on image doesn't seem practical in real time on images better than a dime
 //  use viewBased implementation isntead
 //
 
 #import "ZCSpinLabel.h"
-#import "CGImageRef+AGK+CATransform3D.h"
 
 @implementation ZCSpinLabel
 
@@ -44,21 +40,6 @@
     }
 }
 
-- (void) customAppearDrawingForRect: (CGRect) rect attribute: (ZCTextBlock *) attribute
-{
-    if (attribute.progress <= 0) {
-        return;
-    }
-    CGFloat realProgress = [ZCEasingUtil easeOutWithStartValue:0 endValue:1 time:attribute.progress];
-    UIImage *image = (UIImage *)attribute.customValue;
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSaveGState(context);
-    CGImageRef newImage = CGImageDrawWithCATransform3D_AGK(image.CGImage, CATransform3DMakeRotation(M_PI / 2 * (1 - realProgress), 0, 1, 0), CGPointMake(0, 0), image.size, [UIScreen mainScreen].scale);
-    UIImage *uiImage = [[UIImage alloc] initWithCGImage:newImage];
-    [uiImage drawInRect:attribute.charRect];
-    CGImageRelease(newImage);
-    CGContextRestoreGState(context);
-}
 
 - (void) customViewAppearChangesForAttribute: (ZCTextBlock *) attribute
 {
